980a89e1c9be25ba23bbda6120c4aed4c6e8ad71
Julius Seporaitis <seporaitis@users.noreply.github.com>
2016-02-13 16:15:39 +0000

    Put back the future imports at the top.
    Duh...

--- tests/grammar/test_alter_table.py
+++ tests/grammar/test_alter_table.py
@@ -1,7 +1,6 @@
 # -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
 try:
-    from __future__ import absolute_import, division, print_function, unicode_literals
-
     import unittest
 
     import pyparsing
