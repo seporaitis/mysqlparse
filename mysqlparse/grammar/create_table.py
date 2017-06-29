a07aaa5d6a4cff529aba88f2ae59fd948deb4e59
Julius Seporaitis <julius@seporaitis.net>
2017-06-29 21:36:38 +0100

    Add column specification syntax.

--- mysqlparse/grammar/create_table.py
+++ mysqlparse/grammar/create_table.py
@@ -3,6 +3,17 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from pyparsing import *
 
+from mysqlparse.grammar.column_definition import column_definition_syntax
+from mysqlparse.grammar.utils import stripQuotes
+
+
+#
+# PARTIAL PARSERS
+#
+
+_column_name = Word(alphanums+ "`_").setParseAction(stripQuotes)
+
+_column_specification_syntax = (_column_name.setResultsName("column_name") + column_definition_syntax)
 
 _create_type = CaselessKeyword("TABLE").setResultsName("create_type")
 
@@ -19,11 +30,12 @@ _if_not_exists = Optional(
 #
 # CREATE TABLE SYNTAX
 #
-# Source: https://dev.mysql.com/doc/refman/5.7/en/create-table.html
+# Source: https://dev.mysql.com/doc/refman/5.7/en/create-table.htlmme
 #
 
 create_table_syntax = Forward()
 create_table_syntax <<= (
     (CaselessKeyword("CREATE").setResultsName("statement_type") + _temporary +
-     _create_type + _if_not_exists + Word(alphanums + "`_").setResultsName("table_name"))
+     _create_type + _if_not_exists + Word(alphanums + "`_").setResultsName("table_name")) +
+    (Suppress("(") + delimitedList(Group(_column_specification_syntax).setResultsName("column_specification", listAllMatches=True)) + Suppress(")"))
 )
--- tests/grammar/test_create_table.py
+++ tests/grammar/test_create_table.py
@@ -10,40 +10,60 @@ class CreateTableSyntaxTest(unittest.TestCase):
 
     def test_create_table(self):
         statement = create_table_syntax.parseString("""
-        CREATE TABLE test_test
+        CREATE TABLE test_test (
+          id INT(11) PRIMARY KEY,
+          col_no1 VARCHAR(255) NOT NULL
+        )
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
         self.assertFalse(statement.temporary)
         self.assertTrue(statement.overwrite)
         self.assertEqual(statement.table_name, 'test_test')
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
 
     def test_create_temporary_table(self):
         statement = create_table_syntax.parseString("""
-        CREATE TEMPORARY TABLE test_test
+        CREATE TEMPORARY TABLE test_test (
+          id INT(11) PRIMARY KEY,
+          col_no1 VARCHAR(255) NOT NULL
+        )
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
         self.assertTrue(statement.temporary)
         self.assertTrue(statement.overwrite)
         self.assertEqual(statement.table_name, 'test_test')
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
 
     def test_create_table_overwrite(self):
         statement = create_table_syntax.parseString("""
-        CREATE TABLE IF NOT EXISTS test_test
+        CREATE TABLE IF NOT EXISTS test_test (
+          id INT(11) PRIMARY KEY,
+          col_no1 VARCHAR(255) NOT NULL
+        )
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
         self.assertFalse(statement.temporary)
         self.assertFalse(statement.overwrite)
         self.assertEqual(statement.table_name, 'test_test')
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
 
     def test_create_temporary_table_overwrite(self):
         statement = create_table_syntax.parseString("""
-        CREATE TEMPORARY TABLE IF NOT EXISTS test_test
+        CREATE TEMPORARY TABLE IF NOT EXISTS test_test (
+          id INT(11) PRIMARY KEY,
+          col_no1 VARCHAR(255) NOT NULL
+        )
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
         self.assertTrue(statement.temporary)
         self.assertFalse(statement.overwrite)
         self.assertEqual(statement.table_name, 'test_test')
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
