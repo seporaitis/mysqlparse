abeacff08ea5877fe7089962b37c8e59447c539b
Julius Seporaitis <julius@seporaitis.net>
2017-07-01 18:37:27 +0100

    Add support for table options in create table

--- mysqlparse/grammar/create_table.py
+++ mysqlparse/grammar/create_table.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from pyparsing import *
 
 from mysqlparse.grammar.column_definition import column_definition_syntax
+from mysqlparse.grammar.identifier import identifier_syntax
 from mysqlparse.grammar.utils import stripQuotes
 
 
@@ -11,9 +12,7 @@ from mysqlparse.grammar.utils import stripQuotes
 # PARTIAL PARSERS
 #
 
-_column_name = Word(alphanums+ "`_").setParseAction(stripQuotes)
-
-_column_specification_syntax = (_column_name.setResultsName("column_name") + column_definition_syntax)
+_column_specification_syntax = (identifier_syntax.setResultsName("column_name") + column_definition_syntax)
 
 _create_type = CaselessKeyword("TABLE").setResultsName("create_type")
 
@@ -27,15 +26,18 @@ _if_not_exists = Optional(
     default=True,
 ).setResultsName("overwrite")
 
+_table_option = Word(alphas + "_").setResultsName("key") + Optional(Suppress("=")) + identifier_syntax.setResultsName("value")
+
 #
 # CREATE TABLE SYNTAX
 #
-# Source: https://dev.mysql.com/doc/refman/5.7/en/create-table.htlmme
+# Source: https://dev.mysql.com/doc/refman/5.7/en/create-table.html
 #
 
 create_table_syntax = Forward()
 create_table_syntax <<= (
     (CaselessKeyword("CREATE").setResultsName("statement_type") + _temporary +
      _create_type + _if_not_exists + Word(alphanums + "`_").setResultsName("table_name")) +
-    (Suppress("(") + delimitedList(Group(_column_specification_syntax).setResultsName("column_specification", listAllMatches=True)) + Suppress(")"))
+    (Suppress("(") + delimitedList(Group(_column_specification_syntax).setResultsName("column_specification", listAllMatches=True)) + Suppress(")")) +
+    Optional(ZeroOrMore(Group(_table_option)), default=[]).setResultsName("table_options")
 )
--- tests/grammar/test_create_table.py
+++ tests/grammar/test_create_table.py
@@ -22,13 +22,35 @@ class CreateTableSyntaxTest(unittest.TestCase):
         self.assertEqual(statement.table_name, 'test_test')
         self.assertEqual(statement.column_specification[0].column_name, "id")
         self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(len(statement.table_options), 0)
+
+    def test_create_table_with_options(self):
+        statement = create_table_syntax.parseString("""
+        CREATE TABLE test_test (
+          id INT(11) PRIMARY KEY,
+          col_no1 VARCHAR(255) NOT NULL
+        ) ENGINE=MyISAM AUTO_INCREMENT id
+        """)
+
+        print(statement.table_options)
+        self.assertEqual(statement.statement_type, 'CREATE')
+        self.assertFalse(statement.temporary)
+        self.assertTrue(statement.overwrite)
+        self.assertEqual(statement.table_name, 'test_test')
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(len(statement.table_options), 2)
+        self.assertEqual(statement.table_options[0].key, 'ENGINE')
+        self.assertEqual(statement.table_options[0].value, 'MyISAM')
+        self.assertEqual(statement.table_options[1].key, 'AUTO_INCREMENT')
+        self.assertEqual(statement.table_options[1].value, 'id')
 
     def test_create_temporary_table(self):
         statement = create_table_syntax.parseString("""
         CREATE TEMPORARY TABLE test_test (
           id INT(11) PRIMARY KEY,
           col_no1 VARCHAR(255) NOT NULL
-        )
+        ) ENGINE=MyISAM AUTO_INCREMENT id
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
@@ -37,13 +59,17 @@ class CreateTableSyntaxTest(unittest.TestCase):
         self.assertEqual(statement.table_name, 'test_test')
         self.assertEqual(statement.column_specification[0].column_name, "id")
         self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.table_options[0].key, 'ENGINE')
+        self.assertEqual(statement.table_options[0].value, 'MyISAM')
+        self.assertEqual(statement.table_options[1].key, 'AUTO_INCREMENT')
+        self.assertEqual(statement.table_options[1].value, 'id')
 
     def test_create_table_overwrite(self):
         statement = create_table_syntax.parseString("""
         CREATE TABLE IF NOT EXISTS test_test (
           id INT(11) PRIMARY KEY,
           col_no1 VARCHAR(255) NOT NULL
-        )
+        ) ENGINE=MyISAM AUTO_INCREMENT id
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
@@ -52,13 +78,17 @@ class CreateTableSyntaxTest(unittest.TestCase):
         self.assertEqual(statement.table_name, 'test_test')
         self.assertEqual(statement.column_specification[0].column_name, "id")
         self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.table_options[0].key, 'ENGINE')
+        self.assertEqual(statement.table_options[0].value, 'MyISAM')
+        self.assertEqual(statement.table_options[1].key, 'AUTO_INCREMENT')
+        self.assertEqual(statement.table_options[1].value, 'id')
 
     def test_create_temporary_table_overwrite(self):
         statement = create_table_syntax.parseString("""
         CREATE TEMPORARY TABLE IF NOT EXISTS test_test (
           id INT(11) PRIMARY KEY,
           col_no1 VARCHAR(255) NOT NULL
-        )
+        ) ENGINE=MyISAM AUTO_INCREMENT id
         """)
 
         self.assertEqual(statement.statement_type, 'CREATE')
@@ -67,3 +97,7 @@ class CreateTableSyntaxTest(unittest.TestCase):
         self.assertEqual(statement.table_name, 'test_test')
         self.assertEqual(statement.column_specification[0].column_name, "id")
         self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.table_options[0].key, 'ENGINE')
+        self.assertEqual(statement.table_options[0].value, 'MyISAM')
+        self.assertEqual(statement.table_options[1].key, 'AUTO_INCREMENT')
+        self.assertEqual(statement.table_options[1].value, 'id')
