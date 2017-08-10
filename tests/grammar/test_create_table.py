51079f6758520e24da72cd6becaa475994a2f727
Julius Seporaitis <julius@seporaitis.net>
2017-08-10 22:04:05 +0100

    Add support for PRIMARY KEY constraint.

--- tests/grammar/test_create_table.py
+++ tests/grammar/test_create_table.py
@@ -100,3 +100,64 @@ class CreateTableSyntaxTest(unittest.TestCase):
         self.assertEqual(statement.table_options[0].value, 'MyISAM')
         self.assertEqual(statement.table_options[1].key, 'AUTO_INCREMENT')
         self.assertEqual(statement.table_options[1].value, 'id')
+
+    def test_create_constraint(self):
+        statement = create_table_syntax.parseString("""
+        CREATE TABLE test_test (
+          id INT(11),
+          col_no1 VARCHAR(255) NOT NULL,
+          CONSTRAINT pkey PRIMARY KEY
+        ) ENGINE=MyISAM AUTO_INCREMENT id;
+        """)
+
+        self.assertEqual(statement.statement_type, 'CREATE')
+        self.assertFalse(statement.temporary)
+        self.assertTrue(statement.overwrite)
+        self.assertEqual(statement.table_name, 'test_test')
+
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.constraints[0].symbol, 'pkey')
+        self.assertEqual(statement.constraints[0].index_type, False)
+
+    def test_create_constraint_with_index_type(self):
+        statement = create_table_syntax.parseString("""
+        CREATE TABLE test_test (
+          id INT(11),
+          col_no1 VARCHAR(255) NOT NULL,
+          CONSTRAINT pkey PRIMARY KEY USING BTREE
+        ) ENGINE=MyISAM AUTO_INCREMENT id;
+        """)
+
+        self.assertEqual(statement.statement_type, 'CREATE')
+        self.assertFalse(statement.temporary)
+        self.assertTrue(statement.overwrite)
+        self.assertEqual(statement.table_name, 'test_test')
+
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.constraints[0].symbol, 'pkey')
+        self.assertEqual(statement.constraints[0].index_type, 'BTREE')
+
+    def test_create_constraint_with_index_type_with_index_columns(self):
+        statement = create_table_syntax.parseString("""
+        CREATE TABLE test_test (
+          id INT(11),
+          col_no1 VARCHAR(255) NOT NULL,
+          CONSTRAINT pkey PRIMARY KEY USING BTREE (id, col_no1(16))
+        ) ENGINE=MyISAM AUTO_INCREMENT id;
+        """)
+
+        self.assertEqual(statement.statement_type, 'CREATE')
+        self.assertFalse(statement.temporary)
+        self.assertTrue(statement.overwrite)
+        self.assertEqual(statement.table_name, 'test_test')
+
+        self.assertEqual(statement.column_specification[0].column_name, "id")
+        self.assertEqual(statement.column_specification[1].column_name, "col_no1")
+        self.assertEqual(statement.constraints[0].symbol, 'pkey')
+        self.assertEqual(statement.constraints[0].index_type, 'BTREE')
+        self.assertEqual(statement.constraints[0].index_columns[0].column_name, 'id')
+        self.assertFalse(statement.constraints[0].index_columns[0].length)
+        self.assertEqual(statement.constraints[0].index_columns[1].column_name, 'col_no1')
+        self.assertEqual(statement.constraints[0].index_columns[1].length, 16)
